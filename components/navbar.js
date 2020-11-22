768c0b9646ef28954638262c1e832590dda0c8a5
Amaan Ahmad <amaanahmad582001@gmail.com>
2020-11-22 13:31:29 +0530

    starter for portfolio and blog

---
+++ components/navbar.js
@@ -0,0 +1,5 @@
+import utilStyles from "../styles/utils.module.css";
+
+export default function navbar() {
+  return <>Home | more about me | Blog | Contact</>;
+}
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import styles from "../styles/global.css";
+
+export default function App({ pageProps, Component }) {
+  return <Component {...pageProps} />;
+}
